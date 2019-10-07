28da92a48830c701369deba89587d5c2e7c4ae35
IliaNekleenov <neiluha@mail.ru>
2019-10-07 13:46:41 +0300

    first try

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,5 +1,36 @@
-// Copyright 2018 Your Name <your_email>
+// Copyright 2019 IliaNekleenov <neiluha@mail.ru>
 
 #include <gtest/gtest.h>
 
 #include <header.hpp>
+
+TEST(STACK, push) {
+  Stack<int> st;
+  st.push(1);
+  EXPECT_EQ(st.head(), 1);
+  st.push(2);
+  EXPECT_EQ(st.head(), 2);
+  st.push(3);
+  EXPECT_EQ(st.head(), 3);
+}
+
+TEST(STACK, copy) {
+  Stack<int> st;
+  st.push(1);
+  st.push(2);
+  st.push(3);
+  Stack<int> copy(st);
+  EXPECT_EQ(copy.head(), 3);
+}
+
+TEST(STACK, pop) {
+  Stack<int> st;
+  st.push(1);
+  st.push(2);
+  st.push(3);
+  EXPECT_EQ(st.pop(), 3);
+  EXPECT_EQ(st.head(), 2);
+  EXPECT_EQ(st.pop(), 2);
+  EXPECT_EQ(st.head(), 1);
+  EXPECT_EQ(st.pop(), 1);
+}
